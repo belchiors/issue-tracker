c52f5a3daf7a303737b0d4149addede27d2337be
Belchior Silva <belchiorisv@gmail.com>
2022-10-23 22:15:49 -0300

    Add context class to manage database connection

---
+++ src/Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Persistence
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // TODO: Configure constraints and map entity relationships
+        }
+    }
+}
