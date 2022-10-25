30e990d71b327230c755410ae03249055e5d9437
Belchior Silva <belchiorisv@gmail.com>
2022-10-25 00:10:47 -0300

    Remove TODO comment from OnModelCreating method

--- src/Infrastructure/Persistence/ApplicationDbContext.cs
+++ src/Infrastructure/Persistence/ApplicationDbContext.cs
@@ -3,17 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Persistence
 {
     public class ApplicationDbContext : DbContext
     {
+        public DbSet<Issue> Issues { get; set; }
+        public DbSet<Label> Labels { get; set; }  
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<User> Users { get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            // TODO: Configure constraints and map entity relationships
+            
         }
     }
 }
