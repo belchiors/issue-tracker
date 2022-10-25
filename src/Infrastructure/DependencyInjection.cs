7bf307385efeb7d27a80ab1406f64f14bfaa3d31
Belchior Silva <belchiorisv@gmail.com>
2022-10-25 01:02:59 -0300

    Create extension method to configure database context services

---
+++ src/Infrastructure/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static void AddDbContext(this IServiceCollection services, string connectionString)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                var version = new MySqlServerVersion(new Version(8, 0, 30));
+                options.UseMySql(connectionString, version, null);
+            });
+        }
+    }
+}
