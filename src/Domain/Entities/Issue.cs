2f04fd633bba11dd25e29ecefd2951f8e3f4631a
Belchior Silva <belchiorisv@gmail.com>
2022-10-25 00:11:48 -0300

    Include reference navigation properties

--- src/Domain/Entities/Issue.cs
+++ src/Domain/Entities/Issue.cs
@@ -1,6 +1,7 @@
 ﻿using Domain.Enums;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Reflection.Emit;
 using System.Text;
@@ -13,8 +14,12 @@ namespace Domain.Entities
         public int Id { get; set; }
         public string? Title { get; set; }
         public string? Description { get; set; }
-        public int ProjectId { get; set; }
         public int UserId { get; set; }
+        [Required]
+        public User? User { get; set; }
+        public int ProjectId { get; set; }
+        [Required]
+        public Project? Project { get; set; }
         public IEnumerable<User>? Assignees { get; set; } = new List<User>();
         public IEnumerable<Label>? Labels { get; set; }
         public IssuePriority Priority { get; set; } = IssuePriority.None;
