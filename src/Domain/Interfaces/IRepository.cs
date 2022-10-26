60b95a53044a83cf01e6bf0e26baf6f086941d63
Belchior Silva <belchiorisv@gmail.com>
2022-10-26 01:04:17 -0300

    Add repository interface and its members

---
+++ src/Domain/Interfaces/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Domain.Interfaces;
+
+public interface IRepository<T> where T : class
+{
+    T? FindById(int id);
+    IEnumerable<T> FindAll();
+    IEnumerable<T> FindAll(Predicate<T> predicate);
+    void Insert(T entity);
+    void Update(T entity);
+    void Delete(T entity);
+}
