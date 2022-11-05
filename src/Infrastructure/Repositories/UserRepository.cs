a6e5e14279de622f3b3a4348762efee9c8e769a3
Belchior Silva <belchiorisv@gmail.com>
2022-11-05 20:40:00 -0300

    Implement user's repository

---
+++ src/Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Domain.Entities;
+using Domain.Interfaces;
+using Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories;
+
+public class UserRepository : IRepository<User>
+{
+    private readonly ApplicationDbContext _dbContext;
+
+    public UserRepository(ApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<User?> FindById(Guid id)
+    {
+        return await _dbContext.Users.FindAsync(id);
+    }
+
+    public async Task<IEnumerable<User>> FindAll()
+    {
+        return await _dbContext.Users.ToListAsync();
+    }
+
+    public async Task<IEnumerable<User>> FindAll(Expression<Func<User, bool>> predicate)
+    {
+        return await _dbContext.Users.Where(predicate).ToListAsync();
+    }
+
+    public async Task Insert(User entity)
+    {
+       await _dbContext.Users.AddAsync(entity);
+       await _dbContext.SaveChangesAsync();
+    }
+
+    public async Task Update(User entity)
+    {
+        _dbContext.Users.Update(entity);
+        await _dbContext.SaveChangesAsync();
+    }
+
+    public async Task Delete(User entity)
+    {
+        _dbContext.Users.Remove(entity);
+        await _dbContext.SaveChangesAsync();
+    }
+}
