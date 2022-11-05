2f141357d76a12d47ba0b2f772d2af9c6dd2ecc1
Belchior Silva <belchiorisv@gmail.com>
2022-11-05 19:03:50 -0300

    Add comment entity model

---
+++ src/Domain/Entities/Comment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Domain.Entities
+{
+    public class Comment
+    {
+        public Guid Id { get; set; }
+        public string Text { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        
+        // Navigation property that holds a reference to a single user entity
+        public Guid UserId { get; set; }
+        public User Author { get; set; }
+
+        // Navigation property that holds a reference to a single issue entity
+        public Guid IssueId { get; set; }
+        public Issue Issue { get; set; }
+    }
+}
