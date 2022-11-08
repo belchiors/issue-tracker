74ccb6bcbca6e272cde10da38573390ecfbaa1b3
Belchior Silva <belchiorisv@gmail.com>
2022-11-08 01:54:40 -0300

    Change proxy context

--- src/WebUI/ClientApp/src/setupProxy.js
+++ src/WebUI/ClientApp/src/setupProxy.js
@@ -4,12 +4,9 @@ const { env } = require('process');
 const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
   env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:4883';
 
-const context =  [
-  "/weatherforecast",
-];
-
 module.exports = function(app) {
-  const appProxy = createProxyMiddleware(context, {
+  const appProxy = createProxyMiddleware(
+    '/api', {
     target: target,
     secure: false,
     headers: {
