7284679eb7e6b6c1aeb776323c4071cfcdf5a6a9
Belchior Silva <belchiorisv@gmail.com>
2022-11-09 13:46:08 -0300

    Add a service to handle common authentication operations

---
+++ src/Application/Services/TokenService.cs
@@ -0,0 +1,36 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Domain.Entities;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Application.Services;
+
+public class TokenService
+{
+    private readonly string _securityKey;
+    
+    public TokenService(string securityKey)
+    {
+        _securityKey = securityKey;
+    }
+    
+    public string GenerateToken(User user)
+    {
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var securityKey = Encoding.ASCII.GetBytes(_securityKey);
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(new Claim[]
+            {
+                new Claim(ClaimTypes.Name, user.Id.ToString())
+            }),
+            Expires = DateTime.UtcNow.AddHours(2),
+            SigningCredentials = new SigningCredentials(
+                new SymmetricSecurityKey(securityKey),
+                SecurityAlgorithms.HmacSha256Signature)
+        };
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+        return tokenHandler.WriteToken(token);
+    }
+}
