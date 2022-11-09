f7812f82ab5d1953871911a6919bf9c5051a044e
Belchior Silva <belchiorisv@gmail.com>
2022-11-09 13:47:29 -0300

    Add extension method to configure authentication services

--- src/Application/DependencyInjection.cs
+++ src/Application/DependencyInjection.cs
@@ -1,5 +1,8 @@
+using System.Text;
 using Application.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Application;
 
@@ -8,6 +11,35 @@ public static class DependencyInjection
     public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
         services.AddSingleton<ProjectService>();
+        
+        return services;
+    }
+    
+    /// <summary>
+    /// This method configures authentication services using JTW Bearer.
+    /// </summary>
+    public static IServiceCollection ConfigureAuthenticationServices(this ServiceCollection services, string securityKey)
+    {
+        services.AddSingleton<TokenService>(tokenService => new TokenService(securityKey));
+        
+        services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+
+                    IssuerSigningKey = new SymmetricSecurityKey(
+                        Encoding.ASCII.GetBytes(securityKey))
+                };
+            });
 
         return services;
     }
