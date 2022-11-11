ff44f315d775389aa9f60a5eb02095d817cbd3c2
Belchior Silva <belchiorisv@gmail.com>
2022-11-11 03:10:59 -0300

    Fix controller route path

--- src/WebUI/Controllers/AccountController.cs
+++ src/WebUI/Controllers/AccountController.cs
@@ -9,7 +9,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace WebUI.Controllers;
 
 [ApiController]
-[Route("api[controller]")]
+[Route("api/[controller]")]
 public class AccountController : ControllerBase
 {
     private readonly AccountService _accountService;
