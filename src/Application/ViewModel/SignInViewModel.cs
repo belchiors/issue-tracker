891f9be0e8bc8d479cb01a0a8c93875cf4e4efbd
Belchior Silva <belchiorisv@gmail.com>
2022-11-13 22:33:36 -0300

    Add a different binding model for sign in use case

---
+++ src/Application/ViewModel/SignInViewModel.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Application.ViewModel;
+
+public class SignInViewModel
+{
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+    
+    [Required]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+}
