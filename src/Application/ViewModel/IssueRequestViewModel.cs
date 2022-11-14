e8efb157dcda246c7d0d91c01cc9cd6feda99096
Belchior Silva <belchiorisv@gmail.com>
2022-11-13 23:22:49 -0300

    Refactor to include nullable priority and status

--- src/Application/ViewModel/IssueRequestViewModel.cs
+++ src/Application/ViewModel/IssueRequestViewModel.cs
@@ -6,8 +6,8 @@ public class IssueRequestViewModel
 {
     [Required] public string Title { get; set; }
     public string? Description { get; set; }
-    [Required] public int Priority { get; set; }
-    [Required] public int Status { get; set; }
+    public int? Priority { get; set; }
+    public int? Status { get; set; }
     [Required] public string UserId { get; set; }
     [Required] public string ProjectId { get; set; }
 }
