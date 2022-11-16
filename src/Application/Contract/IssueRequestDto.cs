8eb932246e59ae062c2ad1f560bd2f97f480dcd2
Belchior Silva <belchiorisv@gmail.com>
2022-11-16 18:16:57 -0300

    Make description required

--- src/Application/Contract/IssueRequestDto.cs
+++ src/Application/Contract/IssueRequestDto.cs
@@ -5,7 +5,7 @@ namespace Application.Contract;
 public class IssueRequestDto
 {
     [Required] public string Title { get; set; }
-    public string? Description { get; set; }
+    [Required] public string Description { get; set; }
     public int? Priority { get; set; }
     public int? Status { get; set; }
     [Required] public string ProjectId { get; set; }
