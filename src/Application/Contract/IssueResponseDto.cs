9ec6204e309b1e28edf9612ced835e3aaa747a3c
Belchior Silva <belchiorisv@gmail.com>
2022-11-16 18:18:54 -0300

    Make priority and status optional

--- src/Application/Contract/IssueResponseDto.cs
+++ src/Application/Contract/IssueResponseDto.cs
@@ -7,8 +7,8 @@ public class IssueResponseDto
     public Guid Id { get; set; }
     public string Title { get; set; }
     public string Description { get; set; }
-    public string Priority { get; set; }
-    public string Status { get; set; }
+    public string? Priority { get; set; }
+    public string? Status { get; set; }
     public DateTime CreatedAt { get; set; }
     public DateTime UpdatedAt { get; set; }
     public string Reporter { get; set; }
