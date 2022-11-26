c7f5524136586e473a2748bac9cffe8cc6f601e7
Belchior Silva <belchiorisv@gmail.com>
2022-11-25 23:57:18 -0300

    Add auto mapper profile

---
+++ src/Application/AutoMapperProfile.cs
@@ -0,0 +1,35 @@
+﻿using Application.Contract;
+using AutoMapper;
+using Domain.Entities;
+using Domain.Enums;
+
+namespace Application;
+
+public class AutoMapperProfile : Profile
+{
+    public AutoMapperProfile()
+    {
+        CreateMap<IssueRequestDto, Issue>()
+            .ForMember(dest => dest.Priority, map =>
+                map.MapFrom(src => (IssuePriority) Enum.ToObject(typeof(IssuePriority), src.Priority)))
+            .ForMember(dest => dest.Status, map =>
+            map.MapFrom(src => (IssueStatus) Enum.ToObject(typeof(IssueStatus), src.Status)));
+        
+        CreateMap<Issue, IssueResponseDto>()
+            .ForMember(dest => dest.Priority, map =>
+                map.MapFrom(src => Enum.GetName(typeof(IssuePriority), src.Priority)))
+            .ForMember(dest => dest.Status, map =>
+                map.MapFrom(src => Enum.GetName(typeof(IssueStatus), src.Status)));
+
+        CreateMap<ProjectRequestDto, Project>();
+        
+        CreateMap<Project, ProjectResponseDto>()
+            .ForMember(dest => dest.Issues, map =>
+                map.MapFrom(src => src.Issues.Count()));
+
+        CreateMap<SignUpDto, User>();
+        CreateMap<User, UserDto>()
+            .ForMember(dest => dest.Role, map
+                => map.MapFrom(src => Enum.GetName(typeof(UserRole), src.Role)));
+    }
+}
