ea73f65ee53ad4f1d6888d0ec362991ebd77cb9e
Belchior Silva <belchiorisv@gmail.com>
2022-12-05 10:58:24 -0300

    Add a logout button (WIP)

--- src/WebUI/ClientApp/src/pages/Layout.jsx
+++ src/WebUI/ClientApp/src/pages/Layout.jsx
@@ -1,16 +1,34 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import NavMenu from 'components/NavMenu';
+
+import NavMenu from "components/NavMenu";
+
+import { isAuthenticated, logout } from "services/auth";
 
 function Layout() {
+  const handleLogout = () => {
+    logout();
+    window.location.reload(true);
+  };
+
   return (
     <div className="page">
       <div className="sidebar">
-          <NavMenu />
+        <NavMenu />
       </div>
       <main>
         <div className="top-row px-4">
-          
+          {isAuthenticated ? (
+            <div className="">
+              <button
+                className="btn btn-link"
+                type="button"
+                onClick={handleLogout}
+              >
+                Sign Out
+              </button>
+            </div>
+          ) : null}
         </div>
         <article className="content px-4">
           <Outlet />
